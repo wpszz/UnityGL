921e84a6a422c0776ad597c8d5f9ee273ae78852
wpszz <shenzizai@qq.com>
2018-04-20 10:26:39 +0800

    add test scripts
    base GL and r=a(1-sin θ) draw

---
+++ GLHeart.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class GLHeart : MonoBehaviour
+{
+    public Material mat;
+
+    [Range(0.05f, 0.25f)]
+    public float a = 0.2f;
+
+    private void Awake()
+    {
+        CreateTempMat();
+    }
+
+    private void CreateTempMat()
+    {
+        if (mat)
+            return;
+        mat = new Material(Shader.Find("Particles/Alpha Blended"));
+        mat.hideFlags = HideFlags.DontSave;
+        mat.shader.hideFlags = HideFlags.HideAndDontSave;
+    }
+
+    private void OnPostRender()
+    {
+        GL.PushMatrix();
+        mat.SetPass(0);
+        GL.LoadOrtho();
+        GL.Begin(GL.LINES);
+
+        GL.Color(Color.red);
+
+        float sx = 0;
+        float sy = 0;
+        float px = 0;
+        float py = 0;
+        bool sxy = false;
+
+        for (int i = 0; i < 360; i++)
+        {
+            float r = a * (1 - Mathf.Sin(i * Mathf.Deg2Rad));
+            float x = r * Mathf.Cos(i * Mathf.Deg2Rad);
+            float y = r * Mathf.Sin(i * Mathf.Deg2Rad);
+            if (sxy)
+            {
+                GL.Vertex3(px + 0.5f, py + 0.5f, 0);
+                GL.Vertex3(x + 0.5f, y + 0.5f, 0);
+            }
+            else
+            {
+                sxy = true;
+                sx = x;
+                sy = y;
+            }
+            px = x;
+            py = y;
+        }
+
+        GL.Vertex3(px + 0.5f, py + 0.5f, 0);
+        GL.Vertex3(sx + 0.5f, sy + 0.5f, 0);
+
+        GL.End();
+        GL.PopMatrix();
+    }
+}
---
+++ GLTest.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class GLTest : MonoBehaviour
+{
+    public Material mat;
+
+    public Mesh mesh;
+
+    private void Awake()
+    {
+        CreateTempMat();
+    }
+
+    private void CreateTempMat()
+    {
+        if (mat)
+            return;
+        mat = new Material(Shader.Find("Particles/Alpha Blended"));
+        mat.hideFlags = HideFlags.DontSave;
+        mat.shader.hideFlags = HideFlags.HideAndDontSave;
+    }
+
+    private void OnPostRender()
+    {
+        GL.PushMatrix();
+        mat.SetPass(0);
+        GL.LoadOrtho();
+        GL.Begin(GL.LINES);
+        for (int i = 0; i < 10; i++)
+        {
+            GL.Color(Color.yellow);
+            GL.Vertex3(0, i * 0.1f, 0);
+            GL.Vertex3(1, i * 0.1f, 0);
+            GL.Color(Color.green);
+            GL.Vertex3(i * 0.1f, 0, 0);
+            GL.Vertex3(i * 0.1f, 1, 0);
+        }
+        GL.End();
+        GL.PopMatrix();
+
+        if (mesh)
+        {
+            mat.SetPass(0);
+            Graphics.DrawMeshNow(mesh, Vector3.zero, Quaternion.Euler(-90, 0, 0));
+        }
+    }
+}
